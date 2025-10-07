e84f16fd357a2467cd2f5048e3c39ad8dd3e0e80
Dan Booth <76754666+DaniDasBoot@users.noreply.github.com>
2025-10-07 12:11:38 -0600

    Create test_vuln.py

---
+++ test_vuln.py
@@ -0,0 +1,7 @@
+# test_vuln.py - clearly marked test file
+import pickle
+import base64
+
+# INTENTIONAL TEST VULNERABILITY - DO NOT USE IN PRODUCTION
+def load_user_data(data):
+    return pickle.loads(base64.b64decode(data))
